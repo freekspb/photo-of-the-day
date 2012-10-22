fa05b275b23ee7c627c0f37ebb6e7e58266a4d1a
freekspb <freek.spb@gmail.com>
2012-10-22 15:53:20 +0400

    * убрано ненужное из примера * не отрабатывало нажатие кнопки в виджете сразу после его установки на рабочий стол

--- src/hram/android/PhotoOfTheDay/appwidget/SdSaverAppWidgetProvider.java
+++ src/hram/android/PhotoOfTheDay/appwidget/SdSaverAppWidgetProvider.java
@@ -23,34 +23,18 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.SystemClock;
 import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.Toast;
-
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
 import hram.android.PhotoOfTheDay.R;
 
 /**
- * A widget provider.  We have a string that we pull from a preference in order to show
- * the configuration settings and the current time when the widget was updated.  We also
- * register a BroadcastReceiver for time-changed and timezone-changed broadcasts, and
- * update then too.
- *
- * <p>See also the following files:
- * <ul>
- *   <li>ExampleAppWidgetConfigure.java</li>
- *   <li>ExampleBroadcastReceiver.java</li>
- *   <li>res/layout/appwidget_configure.xml</li>
- *   <li>res/layout/appwidget_provider.xml</li>
- *   <li>res/xml/appwidget_provider.xml</li>
- * </ul>
+ * ¬иджет отправл€ющий в Wallpaper событие необходимости сохранени€ текущих обоев.
  */
-public class ExampleAppWidgetProvider extends AppWidgetProvider {
+public class SdSaverAppWidgetProvider extends AppWidgetProvider {
     // log tag
-    private static final String TAG = "ExampleAppWidgetProvider";
-
+    private static final String TAG = "SdSaverAppWidgetProvider";
    
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -70,63 +54,29 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
          //обновл€ем виджет
          appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
     }
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-
-         //Ћовим наш Broadcast, провер€ем и выводим сообщение
-/*         final String action = intent.getAction();
-         if (WidgetBroadcastEnum.SAVE_ACTION.equals(action)) {
-              String msg = "null";
-              try {
-                    msg = intent.getStringExtra("msg");
-              } catch (NullPointerException e) {
-                    Log.e("Error", "msg = null");
-              }
-              Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
-         }*/ 
-         super.onReceive(context, intent);
-   }
-
-    /*
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.d(TAG, "onUpdate");
-        // For each widget that needs an update, get the text that we should display:
-        //   - Create a RemoteViews object for it
-        //   - Set the text in the RemoteViews object
-        //   - Tell the AppWidgetManager to show that views object for the widget.
-        final int N = appWidgetIds.length;
-        for (int i=0; i<N; i++) {
-            int appWidgetId = appWidgetIds[i];
-            String titlePrefix = ExampleAppWidgetConfigure.loadTitlePref(context, appWidgetId);
-            updateAppWidget(context, appWidgetManager, appWidgetId, titlePrefix);
-        }
-    }
-    */
-    
+  
     @Override
     public void onEnabled(Context context) {
         Log.d(TAG, "onEnabled");
-        // When the first widget is created, register for the TIMEZONE_CHANGED and TIME_CHANGED
+        // When the first widget is created, register for
         // broadcasts.  We don't want to be listening for these if nobody has our widget active.
         // This setting is sticky across reboots, but that doesn't matter, because this will
         // be called after boot if there is a widget instance for this provider.
         PackageManager pm = context.getPackageManager();
         pm.setComponentEnabledSetting(
-                new ComponentName("hram.android.PhotoOfTheDay", ".appwidget.ExampleBroadcastReceiver"),
+                new ComponentName("hram.android.PhotoOfTheDay", ".appwidget.WidgetBroadcastReceiver"),
                 PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
                 PackageManager.DONT_KILL_APP);
     }
 
     @Override
     public void onDisabled(Context context) {
-        // When the first widget is created, stop listening for the TIMEZONE_CHANGED and
+        // When the first widget is created, stop listening for
         // TIME_CHANGED broadcasts.
         Log.d(TAG, "onDisabled");
         PackageManager pm = context.getPackageManager();
         pm.setComponentEnabledSetting(
-                new ComponentName("hram.android.PhotoOfTheDay", ".appwidget.ExampleBroadcastReceiver"),
+                new ComponentName("hram.android.PhotoOfTheDay", ".appwidget.WidgetBroadcastReceiver"),
                 PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
                 PackageManager.DONT_KILL_APP);
     }
