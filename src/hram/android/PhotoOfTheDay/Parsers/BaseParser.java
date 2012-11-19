dc777443e46c12307e47d53c0e90f2dcc816f6e1
hram <hram.ov.e@gmail.com>
2012-11-20 00:53:47 +0400

    + тестовый парсер для проверки работы с "ошибочными" URL + в случае ошибки OutOfMemoryError вывод сообщения об ошибке (сомнительное решение надо подумать как лучше) * обновлена библиотека bugsense	- 3.0.7 * рефакторинг небольшой * закоментировано логирование OutOfMemoryError (логи показали что url корректны) * для экономии памяти парсеры создаются только для скачивания

--- src/hram/android/PhotoOfTheDay/Parsers/BaseParser.java
+++ src/hram/android/PhotoOfTheDay/Parsers/BaseParser.java
@@ -1,14 +1,19 @@
 package hram.android.PhotoOfTheDay.Parsers;
 
 import hram.android.PhotoOfTheDay.Constants;
+import hram.android.PhotoOfTheDay.Wallpaper;
 
 import java.io.IOException;
 import java.util.Random;
+
+import android.content.SharedPreferences;
 
 public abstract class BaseParser 
 {
 	public static final String TAG = Constants.TAG;
 	protected Random rnd = new Random(System.currentTimeMillis());
+	protected SharedPreferences preferences;
+	protected Wallpaper wp;
 	
 	public abstract String GetUrl() throws IOException;
 	
