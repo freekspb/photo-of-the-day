f163a65635e7649021db042c396f577b75208175
hram <hram.ov.e@gmail.com>
2012-11-21 02:24:10 +0400

    возможно версия 2.3 + в настройках программы добавлен пункт "О программе" с переходом на сайт с описанием и отображением текущей версии * убраны предупреждения из манифеста (android:debuggable вроде как не нужен http://stackoverflow.com/questions/4580595/what-would-happen-if-android-app-is-released-with-debuggable-on/4580630#4580630) - для релиза убран тестовый парсер

--- src/hram/android/PhotoOfTheDay/Settings.java
+++ src/hram/android/PhotoOfTheDay/Settings.java
@@ -1,8 +1,12 @@
 package hram.android.PhotoOfTheDay;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 
 public class Settings  extends PreferenceActivity  implements SharedPreferences.OnSharedPreferenceChangeListener 
@@ -18,6 +22,16 @@ public class Settings  extends PreferenceActivity  implements SharedPreferences.
         
         ListPreference sourcesPref = (ListPreference) findPreference(Constants.SOURCES_NAME);
         sourcesPref.setDefaultValue(getPreferenceManager().getSharedPreferences().getString(Constants.SOURCES_NAME, "1"));
+        
+        Preference pref = findPreference("howToUsePref");
+        pref.setOnPreferenceClickListener( new OnPreferenceClickListener() 
+        {
+			public boolean onPreferenceClick(Preference preference) 
+			{
+				ShowInBrowser();
+				return true;
+			}
+		} );
 	}
 	
 	@Override
@@ -38,4 +52,10 @@ public class Settings  extends PreferenceActivity  implements SharedPreferences.
 		
 	}
 	
+	public void ShowInBrowser()
+	{
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://blog.hram0v.com/2012/05/foto-dnya-zhivyie-oboi-ot-yandeks-fotki-flickr-i-na/"));
+        //intent/*.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)*/.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+	}
 }
