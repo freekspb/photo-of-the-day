f1b417ca180a63e4cb13f999f7b7a9d8d05d5c12
freekspb <freek.spb@gmail.com>
2013-02-18 22:26:33 +0400

    * переделана загрузка DieselStation на загрузку с мобильного сайта

--- src/hram/android/PhotoOfTheDay/Parsers/DieselStation.java
+++ src/hram/android/PhotoOfTheDay/Parsers/DieselStation.java
@@ -25,7 +25,7 @@ public class DieselStation extends BaseParser
 	{
 		try
 		{
-			String image = GetUrlBigImage();
+			String image = GetUrlBigImageMobile();
 			if (image != null)
 			{
 				return image;
@@ -84,71 +84,58 @@ public class DieselStation extends BaseParser
 		}catch (OutOfMemoryError e) {}
 		return null;
 	}
-	
-	public String GetUrlBigImage() throws IOException, IncorrectDataFormat
-	{
-		String url = "http://feeds.feedburner.com/dieselstation/txzD";
-		final URL feedUrl = new URL(url);
 		
-		//ArrayList<String> urls = new ArrayList<String>(); 
+	public String GetUrlBigImageMobile() throws IOException, IncorrectDataFormat
+	{
+		String url = "http://m.dieselstation.com/";
+		final String userAgent = "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
 		try {
-			// Создаем фабрику для создания постоителя документов.
-	        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-	        // Непосредственно постhоитель
-	        DocumentBuilder builder = factory.newDocumentBuilder();
-			// Собственно наш документ
-	        org.w3c.dom.Document doc = builder.parse(feedUrl.openConnection().getInputStream());
-	        
-	        org.w3c.dom.Element root = doc.getDocumentElement();
-	        
-	        org.w3c.dom.NodeList items = root.getElementsByTagName("item");
-	        if (items.getLength() <= 0)
-        	{
-        		return null;
-        	}
-	        
-	        org.w3c.dom.Element item = (org.w3c.dom.Element) items.item( 0 );
-        	org.w3c.dom.NodeList links = item.getElementsByTagName("link");
-        	if (links.getLength() <= 0)
-        	{
-        		return null;
-        	}
-        	
-        	org.w3c.dom.Element link = (org.w3c.dom.Element) links.item( 0 );
-    		String str = link.getTextContent();
-
-	        Document doc1 = Jsoup.connect(str)
-				.userAgent("Mozilla")
+	        Document doc0 = Jsoup.connect(url)
+				.userAgent(userAgent)
 				.get();
 	        
-			Element wallPreview = doc1.select("a[class=same_wallpaper]").first();
-			if(wallPreview == null)
+			Element content = doc0.select("div[class=article_content]").first();
+			if(content == null)
+			{
+				throw new IncorrectDataFormat(doc0.ownText());
+			}
+			Element href = content.select("a").first();
+			url = href.attr("href");
+
+	        Document doc1 = Jsoup.connect(url)
+				.userAgent(userAgent)
+				.get();
+	        
+			Element gallery = doc1.select("div[class=gallery]").first();
+			if(gallery == null)
 			{
 				throw new IncorrectDataFormat(doc1.ownText());
 			}
-			String str2 = wallPreview.attr("href");
-			
-	        Document doc2 = Jsoup.connect(str2)
-				.userAgent("Mozilla")
-				.get();
-			Element wallpaper = doc2.select("img[id=wallpaper]").first();
-			if(wallpaper == null)
+			Element galleryItem = gallery.select("div").first();
+			if(galleryItem == null)
 			{
-				throw new IncorrectDataFormat(doc2.ownText());
+				throw new IncorrectDataFormat(doc1.ownText());
 			}
-			return wallpaper.attr("src");
-		}catch (SAXException e) {
-			//try{
-			//	BugSenseHandler.sendExceptionMessage("EarthShots.SAXException", url, new hram.android.PhotoOfTheDay.Exceptions.OutOfMemoryError(e.getMessage()));
-			//}catch (Exception e2) {}
-		}catch (ParserConfigurationException e) {
-			//try{
-			//	BugSenseHandler.sendExceptionMessage("EarthShots.ParserConfigurationException", url, new hram.android.PhotoOfTheDay.Exceptions.OutOfMemoryError(e.getMessage()));
-			//}catch (Exception e2) {}
+			Element galleryItemAttr = galleryItem.select("a").first();
+			if(galleryItemAttr == null)
+			{
+				throw new IncorrectDataFormat(doc1.ownText());
+			}
+			url = galleryItemAttr.attr("href");
+			
+	        Document doc2 = Jsoup.connect(url)
+				.userAgent(userAgent)
+				.get();
+	        String str = doc2.html();
+	        int startPosition = str.indexOf("img id=\"wallpaper\" class=\"img_resize\" src=\"");
+	        startPosition += 43;
+    		int endPosition = str.indexOf("\"", startPosition);
+    		String image = str.substring(startPosition, endPosition);
+			return image;
 		}catch (OutOfMemoryError e) {}
 		return null;
 	}
-		
+
 	@Override
 	public boolean IsTagSupported() 
 	{
